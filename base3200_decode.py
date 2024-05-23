4854a5c0c02aef7ea803bcaaf96a0ef0b3ac1eb1
Shreyas Malhotra <126407864+shreyas-malhotra@users.noreply.github.com>
2024-05-24 03:58:49 +0530

    updated base3200_decode.py

--- base3200_decode.py
+++ base3200_decode.py
@@ -1,12 +1,16 @@
 import base64
 
-input_file = 'base3200_encoded_input_file'
+print("Make sure that this script is being execute in the same directory, that the input file is stored in.")
+
+file_name = input("Please enter the name of the input file: ")
 
 for _ in range(50):
-    with open(input_file, 'r') as f:
+    with open(file_name, 'r') as f:
         encoded_data = f.read().strip()
 
     decoded_data = base64.b64decode(encoded_data).decode('utf-8')
 
-    with open(input_file, 'w') as f:
+    with open(file_name, 'w') as f:
         f.write(decoded_data)
+
+print("The input file should now have be overwritten with the decoded data.")
